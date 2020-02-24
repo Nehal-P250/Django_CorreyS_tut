b4d0e11761724846b85f999fd23872605590a734
vectorNP <37014362+vectorNP@users.noreply.github.com>
2020-02-24 21:24:19 +0530

    vid 7 Login + register+ auth

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 """
 so whenever we enter a url it is first checks in this file if it is found then it redirects it to 
@@ -38,5 +39,8 @@ remaining after we chop off blog so it's just going to send an empty string to b
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register, name='register'),
+    path('profile/',user_views.profile, name='profile'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('',include('blog.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -9,8 +10,18 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request,f'Your account has been created! You are now able to login')
+            return redirect('login')
     else :
         form = UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
+
+
+# the below at-the-rate is an declarator basically it adds functionality 
+# to an existin function here it ensures that the user is loged in before 
+# it can view the profile page
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
+  
